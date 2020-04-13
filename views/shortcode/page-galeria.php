f6d0a7a8cd46539426e92358219d486570e1055c
solucioneswebig <alexanderr677@gmail.com>
2020-04-13 11:41:18 -0400

    Modificado Galeria 1

--- views/shortcode/page-galeria.php
+++ views/shortcode/page-galeria.php
@@ -4,20 +4,25 @@
 
 function validar_direct($id){
 global $wpdb;
+
+global $id_gallery;
+
+$id_gallery = $id;
+
+}
+
+global $id_gallery;
+
 $galeria = $wpdb->get_results("
     SELECT *
     FROM " . TABLE_GALLERY . " as gallery
     LEFT JOIN " . IMG_GALLERY . " as img ON img.id_gallery = gallery.id_gallery
-    WHERE gallery.id_gallery = ".$id."
+    WHERE gallery.id_gallery = ".$id_gallery."
 ");
 
 if ($galeria[0]->type_gallery == 1) {
 add_action('wp_enqueue_scripts', 'gallery_1'); 
 }
-echo "<h1>AQUIIIIIIIIII ".$id."</h1>";
-
-}
-
 
 
 function page_galeria_imagen( $atts ){
